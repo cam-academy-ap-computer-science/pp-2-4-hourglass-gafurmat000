2149c240f53fe5dfdabea7bc680353b33d774afa
GAFUR <GAFUR@192.168.0.29>
2019-10-17 23:36:13 -0700

    Complete

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -25,27 +25,63 @@
 
 public class Hourglass {
 	public static void main(String[] args) {
+		drawLine();
+		drawTop();
+		middle();
+		drawBottom(); 
+		drawLine();
+	}
+	//creates both the top and the bottom of the hour glass/ moves onto next part
+		public static void drawLine() {
 		System.out.print("|");
 		for(int i = 1; i <= 10; i++) {
 			System.out.print("\"");
+			
 	    }
-		System.out.println("|");
-
+		System.out.println("|"); 
+		}
+		//creates the top of the hour glass/ moves onto next part
+		 public static void drawTop() {
+			 for (int line = 1; line <= 4; line++) {
+				 for(int space = 1; space <= 1 * line + 0; space++) {
+					 System.out.print(" ");
+				 }
+				 System.out.print("\\");
+				 for (int i = 1; i <= (10 - 2 * line); i++) {
+					 System.out.print(":");
+				 }
+				 System.out.print("/");
+				 for (int spaces2 = 1; spaces2 <= (line - 2); spaces2++) {
+					 System.out.print(" ");
+			 }
+				 System.out.println(" ");
+			 }
+			 }
 		
-		for(int i = 1; i <= 4; i++) { 
-			System.out.print("\\");	
+		 //creates the middle part of the hour glass in which the sand  goes through/ moves onto next part
+		 public static void middle() {
+			for(int k = 1; k <= 5; k++) {
+			System.out.print(" ");
+			}
+			System.out.println("||");
 		}
-		for(int j = 1; j <= (8 - 6) * 4; j ++) {
-		System.out.print(":");
-		}
-/*		
-		System.out.print("\\");
-		for(int j = 1; j <= ((8 - 2) * i); j++) {
-			System.out.print(":");
-		}
-*/
-	}
+		 //creates the bottom of the hour glass/ and moves onto next part
+		public static void drawBottom() {
+			for (int line = 1; line <= 4; line++) {
+				 System.out.print(" ");
+				 for(int space = 1; space <= -1 * line + 4; space++) {
+					 System.out.print(" ");
+				 }
+				 System.out.print("/");
+				 for (int i = 1; i <= 2 * (line); i++) {
+					 System.out.print(":");
+				 }
+				 System.out.print("\\");
+				 for (int spaces2 = 1; spaces2 <= line ; spaces2++) {
+					 System.out.print(" ");
+			 }
+				 System.out.println(" ");
+			 }
+			 }
 }
-
-
-
+	
